f2241cb28796e2196a6c76e29da84b61caf458b2
Diogo Reis <diogokings@gmail.com>
2015-11-01 17:47:07 -0200

    Update Eleitor.cs

--- Urna/Urna/Eleitor.cs
+++ Urna/Urna/Eleitor.cs
@@ -8,5 +8,29 @@ namespace Urna
 {
     class Eleitor
     {
+        public int IDEleitor { get; set; }
+        public string Nome { get; set; }
+        public string TituloEleitoral { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string ZonaEleitoral { get; set; }
+        public string Secao { get; set; }
+        public char Situacao { get; set; }
+        public char Votou { get; set; }
+
+        public Eleitor(int idEleitor, string nome, string tituloEleitoral, string rg, string cpf, DateTime dataNascimento, string zonaEleitoral, string secao, char situacao, char votou)
+        {
+            this.IDEleitor = idEleitor;
+            this.Nome = nome;
+            this.TituloEleitoral = tituloEleitoral;
+            this.RG = rg;
+            this.CPF = cpf;
+            this.DataNascimento = dataNascimento;
+            this.ZonaEleitoral = zonaEleitoral;
+            this.Secao = secao;
+            this.Situacao = situacao;
+            this.Votou = votou;
+        }
     }
 }
