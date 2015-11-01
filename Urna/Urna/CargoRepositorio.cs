6cfd91ae603da0297a343ee13e8474e1af385b92
emmanoelf <emmanoel_95@hotmail.com>
2015-11-01 17:48:57 -0200

    Adicionado Cargo e CargoRepositorio

--- Urna/Urna/CargoRepositorio.cs
+++ Urna/Urna/CargoRepositorio.cs
@@ -1,12 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Urna
 {
     class CargoRepositorio
     {
+        public void AdicionarCargo(Cargo cargo)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope())
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "INSERT INTO Cargo (Nome, Situacao) values(@paramNome, @paramSituacao)";
+                comando.AddParameter("@paramNome", cargo.Nome);
+                comando.AddParameter("@paramSituacao", cargo.Situacao);
+                connection.Open();
+                comando.ExecuteNonQuery();
+                transacao.Complete();
+                connection.Close();
+            }
+        }
+
+        public void DeletarCargo(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope()) 
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "DELETE FROM Cargo WHERE IDCargo = @paramIDCargo";
+                comando.AddParameter("paramIDCargo", id);
+                connection.Open();
+                comando.ExecuteNonQuery();
+                transacao.Complete();
+                connection.Close();
+            }
+        }
+
+        public void AtivarCargo(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope())
+            using(IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "UPDATE Cargo SET Situacao = A WHERE IDCargo = @paramIDCargo";
+                comando.AddParameter("paramIDCargo", id);
+                connection.Open();
+                comando.ExecuteNonQuery();
+                transacao.Complete();
+                connection.Close();
+            }
+        }
+
     }
 }
