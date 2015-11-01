743df4c8d1952fc02b971f18895bc728193a6fc0
emmanoelf <emmanoel_95@hotmail.com>
2015-11-01 20:03:39 -0200

    Adicionado verificação de cargo existente. Adicionado InativarCargo

--- Urna/Urna/CargoRepositorio.cs
+++ Urna/Urna/CargoRepositorio.cs
@@ -14,19 +14,23 @@ namespace Urna
     {
         public void AdicionarCargo(Cargo cargo)
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
-            using (TransactionScope transacao = new TransactionScope())
-            using (IDbConnection connection = new SqlConnection(connectionString))
+            bool podeCadastrar = PodeCadastrar(cargo);
+            if (podeCadastrar)
             {
-                IDbCommand comando = connection.CreateCommand();
-                comando.CommandText =
-                    "INSERT INTO Cargo (Nome, Situacao) values(@paramNome, @paramSituacao)";
-                comando.AddParameter("@paramNome", cargo.Nome);
-                comando.AddParameter("@paramSituacao", cargo.Situacao);
-                connection.Open();
-                comando.ExecuteNonQuery();
-                transacao.Complete();
-                connection.Close();
+                string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+                using (TransactionScope transacao = new TransactionScope())
+                using (IDbConnection connection = new SqlConnection(connectionString))
+                {
+                    IDbCommand comando = connection.CreateCommand();
+                    comando.CommandText =
+                        "INSERT INTO Cargo (Nome, Situacao) values(@paramNome, @paramSituacao)";
+                    comando.AddParameter("@paramNome", cargo.Nome);
+                    comando.AddParameter("@paramSituacao", cargo.Situacao);
+                    connection.Open();
+                    comando.ExecuteNonQuery();
+                    transacao.Complete();
+                    connection.Close();
+                }
             }
         }
 
@@ -61,6 +65,52 @@ namespace Urna
                 comando.ExecuteNonQuery();
                 transacao.Complete();
                 connection.Close();
+            }
+        }
+
+        public void InativarCargo(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope())
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "UPDATE Cargo SET Situacao = I WHERE IDCargo = @paramIDCargo";
+                comando.AddParameter("paramIDCargo", id);
+                connection.Open();
+                comando.ExecuteNonQuery();
+                transacao.Complete();
+                connection.Close();
+            }
+        }
+
+        public bool PodeCadastrar(Cargo cargo)
+        {
+            int contador = 0;
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (TransactionScope transacao = new TransactionScope())
+            using (IDbConnection connection = new SqlConnection(connectionString)){
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "SELECT count(1) as contador FROM Cargo WHERE Nome = @paramNome or Situacao = @paramSituacao";
+                comando.AddParameter("paramNome", cargo.Nome);
+                comando.AddParameter("paramSituacao", cargo.Situacao);
+                connection.Open();
+                IDataReader reader = comando.ExecuteReader();
+                if (reader.Read())
+                {
+                    contador = Convert.ToInt32(reader["contador"]);
+                }
+                connection.Close();
+            }
+            if(contador == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
