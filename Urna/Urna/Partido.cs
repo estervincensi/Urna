9fd26343cac6b80b9f6f5f7053b8becb01d8e38f
Marvin Santos <marvincenteno23@gmail.com>
2015-11-01 17:06:33 -0200

    Classe partido com seus atributos

--- Urna/Urna/Partido.cs
+++ Urna/Urna/Partido.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Urna
 {
-    class Partido
+    public class Partido
     {
+        public int IDPartido { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public string Slogan { get; set; }
+
+        public Partido(int id,string nome,string sigla,string slogan)
+        {
+            this.IDPartido = id;
+            this.Nome = nome;
+            this.Sigla = sigla;
+            this.Slogan = slogan;
+        }
+
     }
 }
