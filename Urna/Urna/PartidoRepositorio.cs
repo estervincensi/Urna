7e24d2cdc0e546d3c5414edb6a45bae1431c8e12
Marvin Santos <marvincenteno23@gmail.com>
2015-11-01 19:28:13 -0200

    metodo para editar partido e seus auxiliares de vaidação

--- Urna/Urna/PartidoRepositorio.cs
+++ Urna/Urna/PartidoRepositorio.cs
@@ -15,9 +15,10 @@ namespace Urna
 
         public bool CadastrarNovoPartido(Partido partido)
         {
-            bool podeCadastrar = PodeSerCadastrado(partido);
+            bool existeBoBanco = JaExisteNoBanco(partido);
+            bool atendeAosRequisitos = AtendeAosRequisitos(partido);
 
-            if (podeCadastrar)
+            if (!existeBoBanco && atendeAosRequisitos)
             {
 
                 string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
@@ -41,17 +42,30 @@ namespace Urna
                     connection.Close();
                 }
 
-                return true;  // se pode cadastrar ele cadastra e retorna verdadeiro pra dizer que deu certo
+                return true;  // se atende aos requisitos ele cadastra e retorna verdadeiro pra dizer que deu certo
             }
             else
             {
-                return false; // se não pode cadastrar porque ja existe no banco ele retorna false pra dizer que não deu certo
+                return false; // se não pode cadastrar porque ja existe no banco ou não atende aos requisitos ele retorna false pra dizer que não deu certo
             }
             
         }
 
 
-        public bool PodeSerCadastrado(Partido partido)
+        public bool AtendeAosRequisitos(Partido partido)
+        {
+            if ((partido.Nome != "" && partido.Sigla != "" && partido.Slogan != "") && (partido.Nome != null && partido.Sigla != null && partido.Slogan != null))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool JaExisteNoBanco(Partido partido)
         {
             int contador = 0;
 
@@ -72,19 +86,107 @@ namespace Urna
                 {
                     contador = Convert.ToInt32(reader["contador"]);
                  
-                    Console.WriteLine(contador);
+                }
+
+                connection.Close();
+            }
+            if(contador == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+            
+        }
+
+
+        public bool EditarPartido(Partido partido)
+        {
+
+            bool atendeAosRequisitos = AtendeAosRequisitos(partido);
+            int? idPartido = partido.IDPartido;
+            bool existeNoDB = JaExisteNoBanco(partido);       
+
+            if (atendeAosRequisitos && idPartido != null && !existeNoDB)
+            {
+                bool idExisteNoBanco = IdExisteNoBanco(partido.IDPartido);
+
+                if (idExisteNoBanco)
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+
+                    using (TransactionScope transacao = new TransactionScope())
+                    using (IDbConnection connection = new SqlConnection(connectionString))
+                    {
+                        IDbCommand comando = connection.CreateCommand();
+                        comando.CommandText = "UPDATE Partido SET Nome = @paramNome, Sigla = @paramSigla, Slogan = @paramSlogan WHERE IDPartido = @paramIDPartido";
+
+                        comando.AddParameter("paramNome", partido.Nome);
+                        comando.AddParameter("paramSlogan", partido.Slogan);
+                        comando.AddParameter("paramSigla", partido.Sigla);
+                        comando.AddParameter("paramIDPartido", partido.IDPartido);
+
+                        connection.Open();
+
+                        comando.ExecuteNonQuery();
+
+                        transacao.Complete();
+                        connection.Close();
+                    }
+
+                    return true;  // se atende aos requisitos e o id existe na tabela é feito o update e retorna verdadeiro pra dizer que deu certo
+
+                }
+                else
+                {
+                    return false; // se não atende aos requisitos ele retorna false pra dizer que não deu certo
+                }
+
+            }
+            else
+            {
+                return false; // se não atende aos requisitos ele retorna false pra dizer que não deu certo
+            }
+
+
+            
+        }
+
+        public bool IdExisteNoBanco(int idPartido)
+        {
+            int contador = 0;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText = "SELECT count(1) as contador FROM Partido WHERE IDPartido = @paramIDPartido";
+
+                comando.AddParameter("paramIDPartido", idPartido);
+
+                connection.Open();
+
+                IDataReader reader = comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    contador = Convert.ToInt32(reader["contador"]);
+
                 }
 
                 connection.Close();
             }
 
-            if(contador == 0 && (partido.Nome != "" && partido.Sigla != "" && partido.Slogan != "") && (partido.Nome != null && partido.Sigla != null && partido.Slogan != null))
+            if(contador == 0)
             {
-                return true;
+                return false;
             }
             else
             {
-                return false;
+                return true;
             }
         }
     }
