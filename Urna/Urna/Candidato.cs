c8bcdfd7455eb8796f11a37016eac5b3de6db70c
Ester <ester.ines@gmail.com>
2015-11-01 14:18:11 -0200

    classe alterando o IDbCommand, incluindo o  AddParameter

--- Urna/Urna/Candidato.cs
+++ Urna/Urna/Candidato.cs
@@ -8,5 +8,30 @@ namespace Urna
 {
     class Candidato
     {
+        public int IDCandidato { get; set; }
+        public string NomeCompleto { get; set; }
+        public string NomePopular { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string RegistroTRE { get; set; }
+        public int IDPartido { get; set; }
+        public string Foto { get; set; }
+        public int Numero { get; set; }
+        public int IDCargo { get; set; }
+        public bool Exibe { get; set; }
+
+        public Candidato(int idCandidato, string nomeCompleto, string nomePopular, DateTime dataNascimento,
+            string registroTRE, int idPartido, string foto, int numero, int idCargo, bool exibe)
+        {
+            this.IDCandidato = idCandidato;
+            this.NomeCompleto = nomeCompleto;
+            this.NomePopular = nomePopular;
+            this.DataNascimento = dataNascimento;
+            this.RegistroTRE = registroTRE;
+            this.IDPartido = idPartido;
+            this.Foto = foto;
+            this.Numero = numero;
+            this.IDCargo = idCargo;
+            this.Exibe = exibe;
+        }
     }
 }
---
+++ Urna/Urna/IDbCommandExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class IDbCommandExtensions
+    {
+        //IDbCommand.protorype.addParameter = function(nome,value){...}
+        public static void AddParameter(this IDbCommand cmd, string nome, object value)
+        {
+            IDbDataParameter param = cmd.CreateParameter();
+            param.ParameterName = nome;
+            param.Value = value ?? DBNull.Value;
+
+            cmd.Parameters.Add(param);
+        }
+    }
+}
