28764669cea5a4430786f4b5548ced2048fd3adf
Marvin Santos <marvincenteno23@gmail.com>
2015-11-01 23:22:01 -0200

    classe de testes do partido repositorio

---
+++ Urna/UnitTestProjectUrna/UnitTestPartidoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectUrna
+{
+    [TestClass]
+    public class UnitTestPartidoRepositorio
+    {
+        [TestMethod]
+        public void PartidoNaoAtendeAosRequisitos()
+        {
+
+
+        }
+    }
+}
