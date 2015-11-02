49cb65cce73a0d0b92e1f5550b74adf4fa9fd0b2
Marvin Santos <marvincenteno23@gmail.com>
2015-11-02 00:34:25 -0200

    testes da classe partido repositorio

--- Urna/UnitTestProjectUrna/UnitTestPartidoRepositorio.cs
+++ Urna/UnitTestProjectUrna/UnitTestPartidoRepositorio.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Urna;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
 
 namespace UnitTestProjectUrna
 {
@@ -7,10 +12,102 @@ namespace UnitTestProjectUrna
     public class UnitTestPartidoRepositorio
     {
         [TestMethod]
-        public void PartidoNaoAtendeAosRequisitos()
+        public void PartidoNaoAtendeAosRequisitosPorTerNomeNull()
         {
+            Partido partido = new Partido(2,null, "MNY" , "nao faremos nada");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool atendeAosRequi = reposi.AtendeAosRequisitos(partido);
 
+            Assert.IsFalse(atendeAosRequi);
+            
+        }
+
+
+        [TestMethod]
+        public void PartidoNaoAtendeAosRequisitosPorTerSiglaNull()
+        {
+            Partido partido = new Partido(2, "stolers", null, "nao faremos nada");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool atendeAosRequi = reposi.AtendeAosRequisitos(partido);
+
+            Assert.IsFalse(atendeAosRequi);
 
         }
+
+
+        [TestMethod]
+        public void PartidoNaoAtendeAosRequisitosPorTerSiglaVazia()
+        {
+            Partido partido = new Partido(2, "stolers", "", "nao faremos nada");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool atendeAosRequi = reposi.AtendeAosRequisitos(partido);
+
+            Assert.IsFalse(atendeAosRequi);
+
+        }
+
+
+        [TestMethod]
+        public void PartidoNaoAtendeAosRequisitosPorTerNomeVazio()
+        {
+            Partido partido = new Partido(2, "", "MNY", "nao faremos nada");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool atendeAosRequi = reposi.AtendeAosRequisitos(partido);
+
+            Assert.IsFalse(atendeAosRequi);
+
+        }
+
+
+
+        [TestMethod]
+        public void PartidoNaoAtendeAosRequisitosPorTerSloganVazio()
+        {
+            Partido partido = new Partido(2, "stolers", "MNY", "");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool atendeAosRequi = reposi.AtendeAosRequisitos(partido);
+
+            Assert.IsFalse(atendeAosRequi);
+
+        }
+
+
+        [TestMethod]
+        public void PartidoNaoAtendeAosRequisitosPorTerSloganNull()
+        {
+            Partido partido = new Partido(2, "stolers", "MNY", null);
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool atendeAosRequi = reposi.AtendeAosRequisitos(partido);
+
+            Assert.IsFalse(atendeAosRequi);
+
+        }
+
+
+        [TestMethod]
+        public void PartidoJaExisteNoBanco()
+        {
+            ConfigurationManager.AppSettings["App.config"].Clone();
+
+            Partido partido = new Partido(1, "Tribunal Regional Eleitoral", "TRE", "Ética em primeiro lugar");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool jaExiste = reposi.JaExisteNoBanco(partido);
+
+            Assert.IsTrue(jaExiste);
+
+        }
+
+
+        [TestMethod]
+        public void PartidoNaoExisteNoBanco()
+        {
+            Partido partido = new Partido(1, "partido do software", "PDS", "Café em primeiro lugar");
+            PartidoRepositorio reposi = new PartidoRepositorio();
+            bool jaExiste = reposi.JaExisteNoBanco(partido);
+
+            Assert.IsFalse(jaExiste);
+
+        }
+
     }
 }
