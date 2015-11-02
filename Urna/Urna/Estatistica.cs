c06a85b062b3176954c72894c8cde01b0f507ee3
Marvin Santos <marvincenteno23@gmail.com>
2015-11-02 02:23:29 -0200

    incluindo classe estatistica

---
+++ Urna/Urna/Estatistica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urna
+{
+    public class Estatistica
+    {
+        public string NomeCompleto { get; set; }
+        public string NomePopular { get; set; }
+        public int NumeroVotos { get; set; }
+
+    }
+}
