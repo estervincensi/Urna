d49c2e13cdaf2269027ece98de1b7050dba62454
emmanoelf <emmanoel_95@hotmail.com>
2015-11-02 14:50:03 -0200

    adicionado teste de cadastrp de nome vazio e nulo

--- Urna/UnitTestProjectUrna/CargoRepositorioTest.cs
+++ Urna/UnitTestProjectUrna/CargoRepositorioTest.cs
@@ -83,6 +83,24 @@ namespace UnitTestProjectUrna
             bool altera = cargoRep.AtivarCargo(1, true);
             Assert.AreEqual(false, altera);
         }
+
+        [TestMethod]
+        public void NaoCadastraComNomeVazio()
+        {
+            Cargo cargo = new Cargo(12, "", 'I');
+            CargoRepositorio cargoRep = new CargoRepositorio();
+            bool cadastrar = cargoRep.AdicionarCargo(cargo, false);
+            Assert.AreEqual(true, cadastrar);
+        }
+
+        [TestMethod]
+        public void NaoCadastraComNomeNulo()
+        {
+            Cargo cargo = new Cargo(22, null, 'A');
+            CargoRepositorio cargoRep = new CargoRepositorio();
+            bool cadastrar = cargoRep.AdicionarCargo(cargo, false);
+            Assert.AreEqual(true, cadastrar);
+        }
         
     }
 }
